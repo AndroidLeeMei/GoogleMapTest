4a5cfebabcceda1409a84b559d2653912f37f466
LeeMei <boubouian@yahoo.com.tw>
2017-12-07 13:05:51 +0800

    新增定位

--- app/src/main/java/com/example/auser/googlemaptest/MainActivity.java
+++ app/src/main/java/com/example/auser/googlemaptest/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.auser.googlemaptest;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -23,6 +27,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements LocationListener, OnMapReadyCallback {
     static final int MIN_TIME = 1000;
@@ -155,21 +162,65 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         // as you specify a parent activity in AndroidManifest.xml.
 
 
-        switch (item.getItemId()){
+//        switch (item.getItemId()){
+//            case R.id.mark:
+//                map.clear();
+//                map.addMarker(new MarkerOptions());
+//        }
+        switch (item.getItemId()) {
             case R.id.mark:
                 map.clear();
-                map.addMarker(new MarkerOptions());
+                map.addMarker(new MarkerOptions().position(map.getCameraPosition().target).title(getAddress()));
+                break;
+            case R.id.settle:
+                item.setChecked(!item.isChecked());
+                if (item.isChecked()) {
+                    map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                } else {
+                    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                }
+                break;
+            case R.id.traffic:
+                item.setChecked(!item.isChecked());
+                map.setTrafficEnabled(item.isChecked());
+                break;
+            case R.id.currpoint:
+                map.animateCamera(CameraUpdateFactory.newLatLng(currentPoint));
+                break;
+            case R.id.setup:
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+                break;
+            case R.id.quit:
+                finish();
+                break;
+        }
+            return super.onOptionsItemSelected(item);
         }
 
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.currpoint) {
-////            return true;
-////        }
-//        }
-        return super.onOptionsItemSelected(item);
-    }
 
     //===============================================================
+
+    String getAddress() {
+        LatLng target = map.getCameraPosition().target;
+        try {
+            List<Address> addressList = new Geocoder(this, Locale.getDefault()).getFromLocation(target.latitude, target.longitude, 1);
+            if (addressList == null && addressList.size() == 0) {
+                return "查無地址";
+
+            } else {
+                String straddress = "";
+                Address address = addressList.get(0);
+                for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                    straddress += address.getAddressLine(i) + "\n";
+                }
+                return straddress;
+
+            }
+        } catch (Exception e) {
+            return String.format("查無地址 :  緯度：%.4f　　經度：　%.4f", target.latitude, target.longitude);
+        }
+
+    }
+
 }
